612ce8e46637f1f7a4aab84875fbc81c42df4f71
Tam Bi <tamnt.vcu@gmail.com>
2024-10-08 23:39:51 +0700

    set background color

--- SocialApp/Scenses/ViewControllerB.swift
+++ SocialApp/Scenses/ViewControllerB.swift
@@ -12,7 +12,7 @@ class ViewControllerB: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
 
 
