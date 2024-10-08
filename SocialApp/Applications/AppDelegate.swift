052c0036fedc1f6575f527f4bdccf1fa013a8a3a
Andy <giangnt078@gmail.com>
2024-10-08 22:36:04 +0700

    [DEV] - update delegate

--- SocialApp/Applications/AppDelegate.swift
+++ SocialApp/Applications/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = ViewControllerB()
+        window?.rootViewController = ViewControllerA()
         window?.makeKeyAndVisible()
         return true
     }
