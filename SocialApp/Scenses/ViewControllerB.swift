a93dff9591f85fcc9f0c0fad4ef9646271908add
Tam Bi <tamnt.vcu@gmail.com>
2024-10-08 23:40:23 +0700

    set color

--- SocialApp/Scenses/ViewControllerB.swift
+++ SocialApp/Scenses/ViewControllerB.swift
@@ -12,7 +12,7 @@ class ViewControllerB: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .white
+        view.backgroundColor = .red
     }
 
 
